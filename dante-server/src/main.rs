9c3d613e77bfdd628e9ce7d2353e8ddb4d18b3b0
Kartofi <anatoli7707@gmail.com>
2025-05-17 14:36:49 +0300

    Added mongodb lib

--- dante-server/src/main.rs
+++ dante-server/src/main.rs
@@ -1,3 +1,19 @@
+use choki::{ self, Server };
+
 fn main() {
-    println!("Hello, world!");
+    let mut server: Server<u8> = Server::new(Some(10_000_000), None); // Max file upload is 10 MB
+
+    server
+        .get("/", |req, mut res, public_var| {
+            return res.send_string("Wassup");
+        })
+        .unwrap();
+
+    server
+        .listen(3000, None, None, || {
+            println!("Dante server started on port 3000!");
+        })
+        .unwrap();
+
+    Server::<u8>::lock();
 }
