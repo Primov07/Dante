0cc091b20a7ee50ea76566eca7bb69567b507095
Vancho <ivancvetkov116@gmail.com>
2025-05-17 14:05:47 +0300

    MusicControl Library

---
+++ MusicControl/Controls.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using NAudio.Wave;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicControl
+{
+	public static class Controls
+	{
+		private static string BaseURL = "/Song/";//Примерен път "D:\\Music\\Linkin Park- Hybrid Theory [FLAC]\\";
+		private static string SongURL;
+		private static Thread songThread;
+		private static bool ManualStop = false;
+		private static List<string> NextQueue = new List<string>();
+		private static List<string> PrevQueue = new List<string>();
+		private static AudioFileReader audioFile;
+		private static WaveOutEvent output = new WaveOutEvent();
+
+		static public void PlaySong(string SongID) {
+			/// Пуска дадената песен, ако друга песен е пусната я спира и пуска новата
+			ManualStop = false;
+			if (output.PlaybackState == PlaybackState.Playing) {
+				output.Stop();
+			}
+			
+			SongURL = BaseURL + SongID;
+			audioFile = new AudioFileReader(SongURL);
+			output.Init(audioFile);
+			output.Play();
+
+			songThread = new Thread(() => {
+				while (output.PlaybackState != PlaybackState.Stopped)
+				{
+					Console.WriteLine(output.PlaybackState);
+					Console.WriteLine(TimeSpan.FromSeconds(audioFile.CurrentTime.TotalSeconds));
+					Console.WriteLine(output.Volume);
+					Thread.Sleep(3000);
+				}
+				if (!ManualStop) NextSong();
+			});
+			songThread.Start();
+		}
+
+		static public PlaybackState GetPlaybackState() {
+			/// Връща PlaybackState.Playing ако песента е пусната, PlaybackState.Paused ако е на пауза и PlaybackState.Stopped ако е спряна
+			return output.PlaybackState;
+		}
+
+		static public void PauseSong() {
+			/// Пауза на песента
+			if (output.PlaybackState == PlaybackState.Playing) {
+				output.Pause();
+			}
+		}
+
+		static public void StopSong() {
+			/// Спира песента
+			if (output.PlaybackState == PlaybackState.Playing) {
+				//songThread.Abort();
+				//songThread.Suspend();
+				ManualStop = true;
+				PrevQueue.Add(SongURL.Replace(BaseURL, ""));
+				output.Stop();
+			}
+		}
+
+		static public void QueueSong(string SongID) {
+			/// Добавя песен в опашката
+			if (output.PlaybackState == PlaybackState.Stopped || output.PlaybackState == PlaybackState.Paused) {
+				PlaySong(SongID);
+			} else {
+				NextQueue.Add(SongID);
+			}
+		}
+
+		static public void NextSong() {
+			/// Пуска следващата песен от опашката
+			PrevQueue.Add(SongURL.Replace(BaseURL, ""));
+			if (NextQueue.Count > 0) {
+				string nextSong = NextQueue[0];
+				NextQueue.RemoveAt(0);
+				PlaySong(nextSong);
+			} else {
+				Console.WriteLine("No more songs in the queue.");
+			}
+		}
+
+		static public void PreviousSong() {
+			/// Пуска предишната песен от опашката
+			if (PrevQueue.Count > 0) {
+				NextQueue.Insert(0, SongURL.Replace(BaseURL, ""));
+				string prevSong = PrevQueue[PrevQueue.Count - 1];
+				PrevQueue.RemoveAt(PrevQueue.Count - 1);
+				PlaySong(prevSong);
+			} else {
+				Console.WriteLine("No previous song in the queue.");
+			}
+		}
+
+		static public void ResumeSong() {
+			/// Възобновява песента
+			if (output.PlaybackState == PlaybackState.Paused) {
+				output.Play();
+			}
+		}
+
+		static public void VolumeUp(float amount) {
+			/// Увеличава звука, amount е между 0.0 и 1.0
+			if (output.Volume < 1.0f) {
+				output.Volume += amount;
+			}
+		}
+
+		static public void VolumeDown(float amount) {
+			/// Намалява звука, amount е между 0.0 и 1.0
+			if (output.Volume > 0.0f) {
+				output.Volume -= amount;
+			}
+		}
+
+		static public void SeekBackward(double durationInSeconds) {
+			/// Превърта назад песента с определено време
+			audioFile.CurrentTime = TimeSpan.FromSeconds(audioFile.CurrentTime.TotalSeconds - durationInSeconds);
+		}
+
+		static public void SeekForward(double durationInSeconds) {
+			/// Превърта напред песента с определено време
+			audioFile.CurrentTime = TimeSpan.FromSeconds(audioFile.CurrentTime.TotalSeconds + durationInSeconds);
+		}
+	}
+}
---
+++ MusicPlayer/Program.cs
@@ -0,0 +1,33 @@
+﻿using MusicPlayer.CLIDisplay;
+using MusicControl;
+using NAudio.Wave;
+
+namespace MusicPlayer
+{
+    internal class Program
+    {
+        static void Main(string[] args) {
+			//Примерен код
+
+			Controls.PlaySong("06 Runaway.flac");
+			while (true) {
+				var key = Console.ReadKey(true);
+				if (key.Key == ConsoleKey.LeftArrow) {
+					Controls.SeekBackward(5);
+				} else if (key.Key == ConsoleKey.RightArrow) {
+					Controls.SeekForward(5);
+				} else if (key.Key == ConsoleKey.UpArrow) {
+					Controls.VolumeUp(0.01f);
+				} else if (key.Key == ConsoleKey.DownArrow) {
+					Controls.VolumeDown(0.01f);
+				} else if (key.Key == ConsoleKey.Spacebar && Controls.GetPlaybackState()==PlaybackState.Playing) {
+					Controls.PauseSong();
+				} else if (key.Key == ConsoleKey.Spacebar && Controls.GetPlaybackState() == PlaybackState.Paused) {
+					Controls.ResumeSong();
+				}
+			}
+
+			Console.ReadKey();
+        }
+    }
+}
